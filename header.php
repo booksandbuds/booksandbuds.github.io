c45645f964450622e7baebba8b842eb537c30d29
Stefanie Swift <sswift@coursehero.com>
2015-10-14 11:36:19 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+
+echo '<header>
+	<div class="header-books">
+		<img src="img/1.jpeg">
+		<img src="img/2.jpeg">
+		<img src="img/3.jpeg">
+		<img src="img/4.jpeg">
+	</div>
+	<h1>Books and Buds</h1>
+	<div class="menu-bar">
+		<ul>
+			<a href="#"><li>
+				schedule
+			</li></a>
+			<a href="recipes.html"><li>
+				recipes
+			</li></a>
+			<a href="updates.html"><li>
+				updates
+			</li></a>
+			<a href="good-quotes.html"><li>
+				good quotes
+			</li></a>
+		</ul>
+	</div>
+</header>'
+
+?>
